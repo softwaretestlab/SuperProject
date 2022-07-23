224d56bf2285c34ec6122849b820748d1cd520e2
Jagan Etikala <Jagan.Etikala@tabcorp.com.au>
2022-07-23 10:14:11 +1000

    New

---
+++ src/test/java/BasicOfJava/AT03_StringInt.java
@@ -0,0 +1,14 @@
+package BasicOfJava;
+
+import org.junit.Test;
+
+public class AT03_StringInt {
+    @Test
+    public void test01(){
+        //declare local variables
+        int i=10;
+        int j=20;
+        int k = i+j;
+        System.out.println("Value of K is : "+ k);
+    }
+}
