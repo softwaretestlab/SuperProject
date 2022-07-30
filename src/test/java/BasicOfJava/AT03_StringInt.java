6258958ad12498699319a5a72a5583815ce17f3d
Jagan Etikala <Jagan.Etikala@tabcorp.com.au>
2022-07-30 10:01:14 +1000

    javconcepts

--- src/test/java/BasicOfJava/AT03_StringInt.java
+++ src/test/java/BasicOfJava/AT03_StringInt.java
@@ -3,6 +3,7 @@ package BasicOfJava;
 import org.junit.Test;
 
 public class AT03_StringInt {
+
     @Test
     public void test01(){
         //declare local variables
@@ -10,5 +11,16 @@ public class AT03_StringInt {
         int j=20;
         int k = i+j;
         System.out.println("Value of K is : "+ k);
+        System.out.println("total value is : "+(i+j));
+
+    }
+
+    @Test
+    public void test04(){
+        //declare some local varibales
+        String test01= "Software";
+        String test02="TestLab";
+        String test03=test01+test02;
+        System.out.println("Value of test03 is  : "+test03);
     }
 }
